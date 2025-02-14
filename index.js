48a589db623222699f8f928ea04534c4f31045f3
Subodh Chauhan <sk3663@srmist.edu.in>
2025-02-15 00:38:23 +0530

    asefaef

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: ['https://our-client-2-0.vercel.app/', 'http://localhost:3000'],
+    origin: ['https://our-client-2-0.vercel.app', 'http://localhost:3000'],
     methods: ['GET', 'POST', 'DELETE'],
     credentials: true
   }));
