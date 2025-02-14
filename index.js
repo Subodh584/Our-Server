476b7c71716a98771eee0d434e90a82145446cdd
Subodh Chauhan <sk3663@srmist.edu.in>
2025-02-14 10:38:32 +0530

    second commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 import readline from 'readline';
 import cors from 'cors';
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json()); // Middleware to parse JSON data
@@ -22,6 +22,10 @@ async function readFileLines(filePath) {
     }
     return lines;
 }
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to OUR-Server!!");
+});
 
 // API: Get all lines from a file
 app.get("/get-lines/:fileName", async (req, res) => {
